313473357363e131037c56b25183a4187edd9c41
handlecusion <handlecu@gmail.com>
2026-08-15 10:40:55 +0900

    kiwiki 0.2.1

--- Formula/kiwiki.rb
+++ Formula/kiwiki.rb
@@ -2,8 +2,8 @@ class Kiwiki < Formula
   desc "Local-first, agent-operated knowledge wiki framework"
   homepage "https://github.com/handlecusion/kiwiki"
   # bump tag+version on each release (scripts/release-bump.sh <version>)
-  url "https://github.com/handlecusion/kiwiki.git", using: :git, tag: "v0.2.0"
-  version "0.2.0"
+  url "https://github.com/handlecusion/kiwiki.git", using: :git, tag: "v0.2.1"
+  version "0.2.1"
 
   depends_on "python@3.13"
 
